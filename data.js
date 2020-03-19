8b21a905258160217b50c99cc9b72b59946232ea
Sivanesh <sivaneshraina@gmail.com>
2020-03-20 00:48:33 +0530

    Completed Main UI

---
+++ data.js
@@ -0,0 +1,75 @@
+const main = {
+  name: 'Sivanesh',
+  mail: 'sivanesh.code@gmail.com',
+  img:
+    'https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg',
+  // Must make it like that typable like old portfolio version
+  role: ['Full Stack Developer'],
+  // https://ionicons.com/
+  connects: [
+    {
+      name: 'Github',
+      iconName: 'logo-github'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin'
+    },
+    {
+      name: 'StackOverflow',
+      iconName: 'logo-stackoverflow'
+    },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter'
+    },
+    {
+      name: 'npm',
+      iconName: 'logo-npm'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline'
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+    { name: 'Blogs', link: '#blogs' }
+  ]
+};
+
+const projects = [
+  {
+    img: 'https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg',
+    heading: 'Speech to code generation using progressive web apps',
+    description:
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    links: [
+      {name: 'Demo', link: ''},
+      {name: 'Docs', link: ''},
+      {name: 'Github', link: ''},
+    ]
+  },
+  {
+    img: 'https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg',
+    heading: 'Speech to code generation using progressive web apps',
+    description:
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    links: [
+      {name: 'Demo', link: ''},
+      {name: 'Docs', link: ''},
+      {name: 'Github', link: ''},
+    ]
+  },
+  {
+    img: 'https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg',
+    heading: 'Speech to code generation using progressive web apps',
+    description:
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    links: [
+      {name: 'Demo', link: ''},
+      {name: 'Docs', link: ''},
+      {name: 'Github', link: ''},
+    ]
+  },
+]
---
+++ script.js
@@ -0,0 +1,80 @@
+const getDOM = selector => () => {
+  return document.querySelector(selector);
+};
+
+// Values DOM nodes
+const dom = {
+  main: {
+    name: getDOM('#main #name'),
+    mail: getDOM('#main #mail'),
+    img: getDOM('#main #img'),
+    role: getDOM('#main #role'),
+    connects: getDOM('#main #connects'),
+    links: getDOM('#main #links')
+  },
+  projects: getDOM('#projects'),
+  logo: getDOM('#projects-page #logo')
+};
+
+function assignDOM(dom, value, options) {
+  console.log('dom, value, img:', dom, value, img);
+
+  if (options?.isImg) {
+    dom.src = value;
+    return;
+  }
+
+  if (options?.isAdjacent) {
+    dom.insertAdjacentHTML('afterbegin', value);
+  }
+
+  dom.innerHTML = value;
+}
+
+// Assigning
+
+// MAIN
+
+assignDOM(dom.main.name(), main.name);
+assignDOM(dom.main.mail(), main.mail);
+dom.main.mail().href = `mailto:${main.mail}?Subject=Hello%20again`;
+assignDOM(dom.main.img(), main.img, { isImg: true });
+assignDOM(dom.main.role(), main.role);
+// assignDOM(dom.main.links(), main.links)
+
+// External Links (ICONS)
+const connectsDOM = main.connects
+  .map(
+    ({ name, iconName }) =>
+      `<ion-icon name="${iconName}" title="${name}"></ion-icon>`
+  )
+  .join('\n');
+assignDOM(dom.main.connects(), connectsDOM);
+
+// Internal Links
+const getLinks = links =>
+  links
+    .map(({ name, link }) => `<a href="${link}" class="text-pink-500" >${name}</a>`)
+    .map((link, index, links) => index === links.length - 1 ? link: `${link} - `)
+    .join('\n');
+assignDOM(dom.main.links(), getLinks(main.links));
+
+// PROJECTS
+dom.logo().insertAdjacentHTML('afterbegin', `<img src=${main.img} />`);
+
+function getProject(heading, desc, img, links) {
+  const projectDOM = [];
+
+  const linksHTML = getLinks(links);
+  const headingDOM = `<h1>${heading}</h1>`;
+  const descDOM = `<p>${desc}</p>`;
+  const imgDOM = `<img src=${img} />`;
+
+  projectDOM.push('<div>', imgDOM, headingDOM, descDOM, linksHTML, '</div>');
+
+  dom.projects().insertAdjacentHTML('afterbegin', projectDOM.join('\n'));
+}
+
+projects.forEach(({ img, heading, description, links }) =>
+  getProject(heading, description, img, links)
+);
